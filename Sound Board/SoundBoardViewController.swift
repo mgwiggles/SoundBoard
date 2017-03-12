b29033a74f840ff72eb313afc3d35c502ae7d725
Mitch Guzman <mguzman9294@gmail.com>
2017-03-11 20:50:58 -0700

    Basic UI set up

---
+++ Sound Board/SoundBoardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SoundBoardViewController.swift
+//  Sound Board
+//
+//  Created by Mitch Guzman on 3/11/17.
+//  Copyright © 2017 Mitch Guzman. All rights reserved.
+//
+
+import UIKit
+
+class SoundBoardViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var recordButton: NSLayoutConstraint!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
